a1fa5e585d4bb179fbbbec6389b3ee58dd0a0a7b
CWalter50 <walt50@gmail.com>
2023-06-27 11:51:48 -0400

    Restore '6/27/2023, 10:44:32 AM Initial files for MakeCode project'

--- main.py
+++
@@ -1,9 +0,0 @@
-def on_button_pressed_a():
-    basic.show_icon(IconNames.DIAMOND)
-    basic.show_number(input.light_level())
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_b():
-    basic.show_icon(IconNames.YES)
-    basic.show_number(input.sound_level())
-input.on_button_pressed(Button.B, on_button_pressed_b)
